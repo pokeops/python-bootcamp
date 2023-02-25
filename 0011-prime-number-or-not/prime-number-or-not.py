1c17a07b621233db2ee700cdcbb059ba24883473
Rahul Dhingra <dhingra.rahul123@gmail.com>
2023-02-25 19:28:15 +0530

    prime-number-or-not

--- 0011-prime-number-or-not/prime-number-or-not.py
+++ 0011-prime-number-or-not/prime-number-or-not.py
@@ -2,25 +2,26 @@ def main():
     # YOUR CODE GOES HERE
     # Please take input and print output to standard input/output (stdin/stdout)
     # E.g. 'input()/raw_input()' for input & 'print' for output
-    
-    N = int(input("Enter an integer: "))
-    count_f = 0
-    if N%2 == 0:
+
+    N = int(input("Enter a natural number: "))
+    if N == 1:
+        print(N,"is neither prime nor composite")
+    elif N % 2 == 0:
         if N == 2:
             print("YES")
         else:
             print("NO")
     else:
-        for factor in range (1, N+1, 1):
-            if N%factor == 0:
-                count_f = count_f + 1
+        count_f = 0
+        for factor in range(1, N+1, 1):
+            if N % factor == 0:
+                count_f += 1
             if count_f == 2 and factor != N:
                 count_f = 3
-                break
         if count_f == 2:
             print("YES")
         else:
             print("NO")
-            
+
 if __name__ == '__main__':
     main()
