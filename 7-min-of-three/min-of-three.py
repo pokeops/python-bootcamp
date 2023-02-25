c7e5f2de9081e18f920a083a3f2451a9cf8717b1
Rahul Dhingra <dhingra.rahul123@gmail.com>
2023-02-25 14:15:19 +0530

    min-of-three

---
+++ 7-min-of-three/min-of-three.py
@@ -0,0 +1,18 @@
+def main():
+    # YOUR CODE GOES HERE
+    # Please take input and print output to standard input/output (stdin/stdout)
+    # E.g. 'input()/raw_input()' for input & 'print' for output
+    A = int(input("Enter first integer: "))
+    B = int(input("Enter second integer: "))
+    C = int(input("Enter third integer: "))
+    if A <= B and A <= C:
+        print(A)
+    elif B <= A and B <= C:
+        print(B)
+    else:
+        print(C)
+    return 0
+
+
+if __name__ == '__main__':
+    main()
